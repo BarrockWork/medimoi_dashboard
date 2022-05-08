78a7b57ab31b75c042c31fa7219c377536b5050c
Coulibaly Karamoko <k.coulibaly@ecole-ipssi.net>
2022-05-09 00:09:08 +0200

    update drug type crud and back functions

--- src/resources/DrugType/DrugTypeCreate.js
+++ src/resources/DrugType/DrugTypeCreate.js
@@ -1,11 +1,12 @@
 import {Create, SimpleForm, BooleanInput, TextInput, required} from 'react-admin';
 import * as React from "react";
+import { RichTextInput } from 'ra-input-rich-text';
 
 export const DrugTypeCreate = () => (
     <Create>
         <SimpleForm>
-            <TextInput source="name" validate={required()} />
-            <BooleanInput source="isActive" defaultValue="true"/>
+            <TextInput label="name" source="name" validate={required()} />
+            <RichTextInput label="description" source="description" validate={required()}/>
         </SimpleForm>
     </Create>
 );
--- src/resources/DrugType/DrugTypeEdit.js
+++ src/resources/DrugType/DrugTypeEdit.js
@@ -1,11 +1,13 @@
 import {TextInput, BooleanInput, Edit, SimpleForm, required} from 'react-admin';
 import * as React from "react";
+import {RichTextInput} from 'ra-input-rich-text';
 
 export const DrugTypeEdit = () => (
     <Edit>
         <SimpleForm>
             <TextInput disabled label="Id" source="id" />
-            <TextInput source="name" validate={required()} />
+            <TextInput label="name" source="name" validate={required()} />
+            <RichTextInput label="description" source="description" validate={required()}/>
             <BooleanInput source="isActive" />
         </SimpleForm>
     </Edit>
